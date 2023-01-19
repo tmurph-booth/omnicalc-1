49e7a47d256977380389f37300ce9f3c0ba04d78
tmurphbooth <tait@chicagobooth.edu>
2023-01-19 02:22:49 +0000

    Completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,48 @@
-class ApplicationController < ActionController::Base
+include ActionView::Helpers::NumberHelper
 
+class ApplicationController < ActionController::Base
+  def blank_square_form
+    self.render({ :template => "calculation_templates/square_form.html.erb" })
+  end
+
+  def calculate_square
+    @num = params.fetch("userInput")
+    @squareOfNum = @num.to_f ** 2.0
+    self.render({ :template => "calculation_templates/square_results.html.erb" })
+  end
+
+  def blank_square_root_form
+    self.render({ :template => "calculation_templates/square_root_form.html.erb" })
+  end
+
+  def calculate_square_root
+    @num = params.fetch("userInput")
+    @squareRootOfNum = @num.to_f ** 0.5
+    self.render({ :template => "calculation_templates/square_root_results.html.erb" })
+  end
+
+  def blank_random_form
+    self.render({ :template => "calculation_templates/random_form.html.erb" })
+  end
+
+  def calculate_random
+    @numMin = params.fetch("userMin").to_f
+    @numMax = params.fetch("userMax").to_f
+    @randomNum = rand(@numMin..@numMax)
+    self.render({ :template => "calculation_templates/random_results.html.erb" })
+  end
+
+  def blank_payment_form
+    self.render({ :template => "calculation_templates/payment_form.html.erb" })
+  end
+
+  def payment_results
+    @numAPR = number_to_percentage(params.fetch("userApr"), precision: 4)
+    monthlyApr = params.fetch("userApr").to_f / 12 / 100
+    @numYears = params.fetch("userYears")
+    months = @numYears.to_f * 12
+    @numPV = number_to_currency(params.fetch("userPV").to_f)
+    @paymentOutput = number_to_currency(params.fetch("userPV").to_f * (monthlyApr * (1 + monthlyApr) ** months) / (((1 + monthlyApr) ** months) - 1))
+    self.render({ :template => "calculation_templates/payment_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-
+  # get("",{:controller => "", :action => ""})
+  get("/square/results", { :controller => "application", :action => "calculate_square" })
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+  get("/square_root/results", { :controller => "application", :action => "calculate_square_root" })
+  get("/square_root/new", { :controller => "application", :action => "blank_square_root_form" })
+  get("/random/new", { :controller => "application", :action => "blank_random_form" })
+  get("/random/results", { :controller => "application", :action => "calculate_random" })
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
+  get("/payment/results", { :controller => "application", :action => "payment_results" })
 end
